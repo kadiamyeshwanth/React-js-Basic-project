19ad517de3ef1c0eb17c8867283e801b46fa3ee4
deadpool1002 <kadiamyeshwanth@gmail.com>
2025-08-15 14:52:18 +0530

    c
    c

---
+++ src/components/Foreground.jsx
@@ -0,0 +1,20 @@
+import React, { useRef } from "react"
+import Card from "./Card"
+
+function Foreground() {
+  const ref = useRef(null)
+  const data = [
+    { desc: "Today i should finish my Dsa work.", filesize: ".9mb", close: true, tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" } },
+    { desc: "Today i should finish my Dsa work.", filesize: ".9mb", close: true, tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" } },
+    { desc: "Today i should finish my Dsa work.", filesize: ".9mb", close: true, tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" } }
+  ]
+  return (
+    <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5">
+      {data.map((item, index) => (
+        <Card key={index} data={item} reference={ref} />
+      ))}
+    </div>
+  )
+}
+
+export default Foreground
